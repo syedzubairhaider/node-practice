26594ceda586f9ce45dc9301d8fc2f1dd551f8c9
zubair <zubair@zubair.localdomain>
2019-08-22 14:14:59 +0500

    improving code

--- src/index.js
+++ src/index.js
@@ -2,63 +2,22 @@ import "dotenv/config";
 
 import cors from "cors";
 import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 
-import models, { sequelize } from "./models";
+import schema from "./schema";
+import resolvers from "./resolvers";
+import models from "./models";
 
 const app = express();
 
 app.use(cors());
-
-const schema = gql`
-  type Query {
-    users: [User!]
-    me: User
-    user(id: ID!): User
-  }
-
-  type User {
-    id: ID!
-    username: String!
-  }
-`;
-
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch1"
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids"
-  }
-};
-
-const me = users[1];
-
-const resolvers = {
-  Query: {
-    me: () => {
-      return me;
-    },
-
-    user: (parent, { id }) => {
-      return users[id];
-    },
-
-    users: () => {
-      return Object.values(users);
-    }
-  },
-
-  User: {
-    username: () => "Hans"
-  }
-};
-
 const server = new ApolloServer({
   typeDefs: schema,
-  resolvers
+  resolvers,
+  context: {
+    models,
+    me: models.users[1]
+  }
 });
 
 server.applyMiddleware({ app, path: "/graphql" });
